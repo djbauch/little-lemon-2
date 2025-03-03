3ce22baef29694396926a4f4ee8cae43124a583a
DJ Bauch <dj.bauch@gmail.com>
2025-03-02 21:58:59 -0600

    Trying to get Jest to run

---
+++ src/components/BookingForm.test.tsx
@@ -0,0 +1,20 @@
+import BookingForm from './BookingForm'
+import {render} from '@testing-library/react'
+
+test('BookingForm renders', () => {
+  const availableTimes = ['17:00', '18:00', '19:00', '20:00', '21:00', '22:00']
+  const setAvailableTimes = jest.fn()
+  const { getByText, getByLabelText } = render(<BookingForm availableTimes={availableTimes} setAvailableTimes={setAvailableTimes} />)
+  const heading = getByText(/Reserve a Table/i)
+  const dateLabel = getByLabelText(/Choose date/i)
+  const timeLabel = getByLabelText(/Choose time/i)
+  const guestsLabel = getByLabelText(/Number of guests/i)
+  const occasionLabel = getByLabelText(/Occasion/i)
+  const submitButton = getByText(/Make your reservation/i)
+  expect(heading).toBeInTheDocument()
+  expect(dateLabel).toBeInTheDocument()
+  expect(timeLabel).toBeInTheDocument()
+  expect(guestsLabel).toBeInTheDocument()
+  expect(occasionLabel).toBeInTheDocument()
+  expect(submitButton).toBeInTheDocument()
+})
--- src/components/BookingForm.tsx
+++ src/components/BookingForm.tsx
@@ -1,32 +1,38 @@
 import * as React from 'react'
 
-const BookingForm = () => {
+interface BookingFormProps {
+  availableTimes: string[]
+  setAvailableTimes: React.Dispatch<React.SetStateAction<string[]>>
+}
+
+const BookingForm = (props: BookingFormProps) => {
+  const { availableTimes } = props
   return (
-    <form style={{display: 'grid', maxWidth: '200px', gap: '20px'}}>
-      <label htmlFor='res-date'>Choose date</label>
-      <input type='date' id='res-date' />
-      <label htmlFor='res-time'>Choose time</label>
-      <select id='res-time'>
-        <option>17:00</option>
-        <option>18:00</option>
-        <option>19:00</option>
-        <option>20:00</option>
-        <option>21:00</option>
-        <option>22:00</option>
-      </select>
-      <label htmlFor='res-guests'>Number of guests</label>
-      <input type='number' placeholder='1'min='1' max='10' id='guests' />
-      <label htmlFor='occasion'>Occasion</label>
-      <select id='occasion'>
-        <option>None</option>
-        <option>Birthday</option>
-        <option>Anniversary</option>
-        <option>Wedding</option>
-        <option>Graduation</option>
-        <option>Other</option>
-      </select>
-      <input type='submit' value='Make your reservation' />
-    </form>
+    <>
+      <h1>Reserve a Table</h1>
+      <form style={{ display: 'grid', maxWidth: '200px', gap: '20px' }}>
+        <label htmlFor='res-date'>Choose date</label>
+        <input type='date' id='res-date' />
+        <label htmlFor='res-time'>Choose time</label>
+        <select id='res-time'>
+          {availableTimes.map((time: string) => (
+            <option key={time}>{time}</option>
+          ))}
+        </select>
+        <label htmlFor='res-guests'>Number of guests</label>
+        <input type='number' placeholder='1' min='1' max='10' id='guests' />
+        <label htmlFor='occasion'>Occasion</label>
+        <select id='occasion'>
+          <option>None</option>
+          <option>Birthday</option>
+          <option>Anniversary</option>
+          <option>Wedding</option>
+          <option>Graduation</option>
+          <option>Other</option>
+        </select>
+        <input type='submit' value='Make your reservation' />
+      </form>
+    </>
   )
 }
 
