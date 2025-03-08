b2bd7671cb9aa77eca6a6090f8178e742853be0c
DJ Bauch <dj.bauch@gmail.com>
2025-03-08 16:14:41 -0600

    Fixed Header and toolbar behavior

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,10 +1,35 @@
+import { useState, useEffect } from 'react'
 import Nav from './Nav'
-
+import { AppBar, Toolbar, Slide, useScrollTrigger } from '@mui/material'
 export const Header = () => {
+  useScrollTrigger()
+  const [show, setShow] = useState(true)
+  const [lastScrollY, setLastScrollY] = useState(0)
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const currentScrollY = window.scrollY
+      if (currentScrollY > lastScrollY) {
+        setShow(false) // Scrolling down
+      } else {
+        setShow(true) // Scrolling up
+      }
+      setLastScrollY(currentScrollY)
+    }
+
+    window.addEventListener('scroll', handleScroll)
+    return () => window.removeEventListener('scroll', handleScroll)
+  }, [lastScrollY])
+
   return (
-    <header>
-      <Nav />
-    </header>
+    <Slide in={show} direction='down'>
+      <AppBar position='sticky'>
+        <Toolbar sx={{ justifyContent: 'space-between', backgroundColor: 'white' }}>
+          <img src='/Logo.svg' alt='Logo' style={{ height: 40 }} />
+          <Nav />
+        </Toolbar>
+      </AppBar>
+    </Slide>
   )
 }
 
--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,31 +1,76 @@
-import { NavLink }  from 'react-router'
+import { useState } from 'react'
+import { NavLink } from 'react-router'
+import IconButton from '@mui/material/IconButton'
+import MenuIcon from '@mui/icons-material/Menu'
+import Drawer from '@mui/material/Drawer'
+import List from '@mui/material/List'
+import ListItem from '@mui/material/ListItem'
+import ListItemButton from '@mui/material/ListItemButton'
+import ListItemText from '@mui/material/ListItemText'
+import Box from '@mui/material/Box'
 
 export const Nav = () => {
-  return (
-    <nav className='nav-bar'>
-      <img src='/Logo.svg' alt='Logo' />
+  const [drawerOpen, setDrawerOpen] = useState(false)
 
-      <ul className='navbar-nav'>
-        <li className='nav-item'>
-          <NavLink to='/'>Home</NavLink>
-        </li>
-        <li className='nav-item'>
-          <NavLink to='/about'>About</NavLink>
-        </li>
-        <li className='nav-item'>
-          <NavLink to='/menu'>Menu</NavLink>
-        </li>
-        <li className='nav-item'>
-          <NavLink to='/reservations'>Reservations</NavLink>
-        </li>
-        <li className='nav-item'>
-          <NavLink to='/order'>Order&nbsp;Online</NavLink>
-        </li>
-        <li className='nav-item'>
-          <NavLink to='/login'>Login</NavLink>
-        </li>
-      </ul>
-    </nav>
+  const toggleDrawer = (open: boolean) => () => {
+    setDrawerOpen(open)
+  }
+
+  const menuItems = [
+    { label: 'Home', path: '/' },
+    { label: 'About', path: '/about' },
+    { label: 'Menu', path: '/menu' },
+    { label: 'Reservations', path: '/reservations' },
+    { label: 'Order Online', path: '/order' },
+    { label: 'Login', path: '/login' },
+  ]
+
+  return (
+    <>
+      <IconButton
+        edge='start'
+        color='inherit'
+        aria-label='menu'
+        sx={{ display: { xs: 'flex', md: 'none' } }}
+        onClick={toggleDrawer(true)}
+      >
+        <MenuIcon />
+      </IconButton>
+
+      <Drawer anchor='left' open={drawerOpen} onClose={toggleDrawer(false)}>
+        <List>
+          {menuItems.map((item) => (
+            <ListItem key={item.label} disablePadding>
+              <ListItemButton component={NavLink} to={item.path} onClick={toggleDrawer(false)}>
+                <ListItemText primary={item.label} />
+              </ListItemButton>
+            </ListItem>
+          ))}
+        </List>
+      </Drawer>
+
+      <Box
+        component='nav'
+        sx={{
+          display: { xs: 'none', md: 'flex' },
+          gap: 2,
+        }}
+      >
+        {menuItems.map((item) => (
+          <NavLink
+            key={item.label}
+            to={item.path}
+            style={{
+              textDecoration: 'none',
+              color: 'inherit',
+              padding: '8px 16px',
+            }}
+          >
+            {item.label}
+          </NavLink>
+        ))}
+      </Box>
+    </>
   )
 }
 
